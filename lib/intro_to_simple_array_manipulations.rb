624dc7ecaf4392a72545afa64b1e25dda62ed2f6
Spencer Blum <spencerblum@gmail.com>
2020-01-17 22:24:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,8 +8,8 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
-def using_pop(@continents)
-  @continents.pop
+def using_pop(continents)
+  continents.pop
 end
 
 def pop_with_args()
